9dfaf12ee9300a3710a70271f549bed43f4efbba
Robert <hc.debest@gmail.com>
2022-10-21 10:54:57 +0200

    switching

---
+++ solution.js
@@ -0,0 +1,5 @@
+const jsonFile = require("./JSON.js");
+
+console.log(jsonFile.JsonFile()[0].address)
+
+
---
+++ tests.js
@@ -0,0 +1,8 @@
+const should = require('chai').should();
+const solution = require('solution');
+
+describe('should do a lot of parsing thing', () =>{
+    it('should console log JSON string',() => {
+        console.log()
+    });
+})
