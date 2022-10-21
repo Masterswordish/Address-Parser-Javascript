d4a96e7f5f9718bf09a5b8d35fbe4cb3998d99ef
salt-dev <salt-dev@salt-devs-MacBook-Air-12.local>
2022-10-21 11:07:01 +0200

    “switching”

--- solution.js
+++ solution.js
@@ -1,5 +1,8 @@
 const jsonFile = require("./JSON.js");
 
+const findPeopleInCity = (city) => {
+    
+}
 console.log(jsonFile.JsonFile()[0].address)
 
 
--- tests.js
+++ tests.js
@@ -2,7 +2,8 @@ const should = require('chai').should();
 const solution = require('solution');
 
 describe('should do a lot of parsing thing', () =>{
-    it('should console log JSON string',() => {
-        console.log()
+    it('should read  JSON data',() => {
+        const city = 'Berkhout';
+        const data = solution.findPeopleInCity(city);
     });
 })
