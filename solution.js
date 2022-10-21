600af293e92f54109b00e0f0d42f2f3d51457822
Robert <hc.debest@gmail.com>
2022-10-21 12:45:52 +0200

    switching

--- solution.js
+++ solution.js
@@ -1,17 +1,18 @@
 const jsonFile = require("./JSON.js");
 
-const findPeopleInCity = (inputCity) => {
+const findPeopleInCityandSortByAge = (inputCity) => {
     const data = jsonFile.JsonFile();
     const result = data
         .filter(p => p.address.city === inputCity)
         .sort((a,b) => b.Age -  a.Age);
     console.log(result);
 }
+
 const findMostPopulatedCity = (inputCity) => {
     const data = jsonFile.JsonFile();
     const result = data
         .reduce()
-    
 }
 
-findPeopleInCity('Amsterdam');
+
+findPeopleInCityandSortByAge('Amsterdam');
