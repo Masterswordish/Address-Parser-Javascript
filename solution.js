bd223eacdcf7c0a8b7ddb222a2c4ef9e670182d7
salt-dev <salt-dev@salt-devs-MacBook-Air-12.local>
2022-10-21 11:57:39 +0200

    “switching”

--- solution.js
+++ solution.js
@@ -4,8 +4,14 @@ const findPeopleInCity = (inputCity) => {
     const data = jsonFile.JsonFile();
     const result = data
         .filter(p => p.address.city === inputCity)
-        .sort((a,b) =>);
+        .sort((a,b) => b.Age -  a.Age);
     console.log(result);
+}
+const findMostPopulatedCity = (inputCity) => {
+    const data = jsonFile.JsonFile();
+    const result = data
+        .reduce()
+    
 }
 
 findPeopleInCity('Amsterdam');
